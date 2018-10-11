72008ff100bc6cfc2cc3485d2a315e5c564ffe4f
Felix Ayala <felix1262@gmail.com>
2018-10-11 16:14:13 -0300

    Update select element in forms

--- login.php
+++ login.php
@@ -1,81 +1,63 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<!-- Required meta tags -->
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<div class="container">		
+	<div class="columns is-centered mt-6">
+		<div class="column is-one-third is-centered">
+			<div class="card card-shadow">
+				<div class="card-content">
+					<h5 class="pb-3 mb-4 border-bottom">Login</h5>
 
-		<!-- App Style -->
-		<link rel="stylesheet" href="css/app.css?<?php echo time(); ?>">
+					<form method="POST">
+					 
+						<div class="field">
+							<label for="email" class="label">Email Address</label>
 
-		<!-- Feather Icons -->
-		<script src="https://unpkg.com/feather-icons"></script>
-	</head>
-	<body>
-		<div class="container">		
-			<div class="columns is-centered mt-6">
-				<div class="column is-one-third is-centered">
-					<div class="card card-shadow">
-						<div class="card-content">
-							<h5 class="pb-3 mb-4 border-bottom">Login</h5>
+							<p class="control has-icons-left">
+								<input class="input" type="email" id="email" placeholder="Email">
 
-							<form method="POST">
-							 
-								<div class="field">
-									<label for="email" class="label">Email Address</label>
-
-									<p class="control has-icons-left">
-										<input class="input" type="email" id="email" placeholder="Email">
-
-										<span class="icon is-small is-left">
-											<i data-feather="mail"></i>
-										</span>
-									</p>
-								</div>
-
-								<div class="field mt-5">
-									<label class="label" for="password">
-										Password
-
-										<a href="forgot.html" class="is-pulled-right">
-											Forgot Password?
-										</a>
-									</label>
-
-									<p class="control has-icons-left">
-										<input id="password" type="password" class="input" name="password">
-
-										<span class="icon is-small is-left">
-											<i data-feather="lock"></i>
-										</span>
-									</p>
-								</div>
-
-								<div class="field mt-5">
-									<button type="submit" class="button is-primary is-fullwidth">
-										Login
-									</button>
-								</div>
-
-								<div class="has-text-centered">
-									Don't have an account? <a href="register.html">Create One</a>
-								</div>
-							</form>
+								<span class="icon is-small is-left">
+									<i data-feather="mail"></i>
+								</span>
+							</p>
 						</div>
-					</div>
-					<!-- END card -->
 
-					<div class="footer has-text-centered p-4">
-						Copyright &copy; <?php echo date('Y'); ?>
-					</div>
-					<!-- END footer -->
+						<div class="field mt-5">
+							<label class="label" for="password">
+								Password
+
+								<a href="forgot.html" class="is-pulled-right">
+									Forgot Password?
+								</a>
+							</label>
+
+							<p class="control has-icons-left">
+								<input id="password" type="password" class="input" name="password">
+
+								<span class="icon is-small is-left">
+									<i data-feather="lock"></i>
+								</span>
+							</p>
+						</div>
+
+						<div class="field mt-5">
+							<button type="submit" class="button is-primary is-fullwidth">
+								Login
+							</button>
+						</div>
+
+						<div class="has-text-centered">
+							Don't have an account? <a href="register.html">Create One</a>
+						</div>
+					</form>
 				</div>
-				<!-- END column -->
 			</div>
-			<!-- END columns -->
+			<!-- END card -->
+
+			<div class="footer has-text-centered p-4">
+				Copyright &copy; <?php echo date('Y'); ?>
+			</div>
+			<!-- END footer -->
 		</div>
-		<!-- END container -->
-		
-		<script src="js/app.js"></script>
-	</body>
-</html>
+		<!-- END column -->
+	</div>
+	<!-- END columns -->
+</div>
+<!-- END container -->
---
+++ navbar.php
@@ -0,0 +1,35 @@
+<nav class="navbar is-dark">
+	<div class="navbar-brand">
+		<a class="navbar-item" href="http://bulma.io">
+			<h1 class="subtitle has-text-white">AMP</h1>
+		</a>
+		<div class="navbar-burger burger" data-target="navMenubd-example">
+			<span></span>
+			<span></span>
+			<span></span>
+		</div>
+	</div>
+	<!-- END navbar-brand -->
+
+	<div class="navbar-menu">
+		<div class="navbar-end">
+			<div class="navbar-item has-dropdown is-hoverable">
+				<a class="navbar-link" href="/documentation/overview/start/">
+					<figure class="image is-24x24 mr-4">
+						<img class="is-rounded" src="https://bulma.io/images/placeholders/32x32.png">
+					</figure>
+					<span>User</span>
+				</a>
+				<div class="navbar-dropdown is-right is-active">
+					<a class="navbar-item has-text-danger" href="https://bulma.io/documentation/elements/box/">
+						<i class="mr-4" data-feather="log-out"></i>
+						<span>Logout</span>
+					</a>
+				</div>
+			</div>
+		</div>
+		<!-- END navbar-end -->
+	</div>
+	<!-- END navbar-menu -->
+</nav>
+<!-- END navbar -->
