47894cfe6108bc3350a40fd3da0ff5535ff45ec2
Felix Ayala <felix1262@gmail.com>
2018-11-05 20:06:48 -0300

    Make visual improvements to navbar dropdown

--- navbar.php
+++ navbar.php
@@ -18,9 +18,16 @@
 					<figure class="image is-24x24 mr-4">
 						<img class="is-rounded" src="https://bulma.io/images/placeholders/32x32.png">
 					</figure>
-					<span>User</span>
+					<span>Felix</span>
 				</a>
 				<div class="navbar-dropdown is-right is-active">
+					<a class="navbar-item">
+						<span>Mi Perfil</span>
+						<span class="helper">felix1262@gmail.com</span>
+					</a>
+
+      				<hr class="navbar-divider">
+
 					<a class="navbar-item">
 						<span>Notifications</span>
 					</a>
@@ -30,8 +37,6 @@
 					<a class="navbar-item">
 						<span>Configuration</span>
 					</a>
-
-      				<hr class="navbar-divider">
 
 					<a class="navbar-item has-text-danger">
 						<span>Logout</span>
